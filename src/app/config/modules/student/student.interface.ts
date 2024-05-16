b01bc13c15339aecadaf5a11280e822e648b3d0d
noyon-podder <noyon.podder7@gmail.com>
2024-05-16 13:44:20 +0600

    student interface create

---
+++ src/app/config/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export type Guardian = {
+  fathersName: string
+  fatherOccupation: string
+  fatherContactNo: string
+  motherName: string
+  motherOccupation: string
+  motherContactNo: string
+}
+
+export type Username = {
+  firstName: string
+  middleName?: string
+  lastName: string
+}
+
+export type LocalGuardian = {
+  name: string
+  occupation: string
+  contactNo: string
+  address: string
+}
+
+export type Student = {
+  name: Username
+  email: string
+  contactNo: string
+
+  gender: 'male' | 'female'
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-'
+  emergencyContactNo: string
+  dateOfBirth: string
+  presentAddress: string
+  permanentAddress: string
+  guardian: Guardian
+  LocalGuardian: LocalGuardian
+  profileImage?: string
+  isActive: 'active' | 'inactive'
+}
