7d839d45679c57d8ac4d45fd02403131faaf5bb5
noyon-podder <noyon.podder7@gmail.com>
2024-05-16 13:05:58 +0600

    server console add

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './app/config'
 
 // connect database
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    mongoose.connection.on('connected', () => {
+      console.log('Database Connection Successfully 💕')
+    })
+
+    mongoose.connection.on('error', (err) => {
+      console.error('Database connection internally Failed: ', err)
+    })
+
+    await mongoose.connect(config.database_url as string)
 
     app.listen(config.port, () => {
-      console.log(`server running ${config.port}`);
-    });
+      console.log(`server running ${config.port}`)
+    })
   } catch (err) {
-    console.log(err);
+    console.log('Database connection failed: ', err)
+    process.exit(1)
   }
 }
 
-main();
+main()
