efe22c7c238d71561c0b74055d817baba816d368
noyon-podder <noyon.podder7@gmail.com>
2024-06-17 02:01:32 +0600

    zod error handler add

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,13 +1,35 @@
-import { ErrorRequestHandler } from 'express'
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  res.status(500).json({
-    success: false,
-    message: err.message,
-  })
+import { ErrorRequestHandler } from 'express'
+import { ZodError } from 'zod'
+import handleZodError from '../errors/handleZodError'
+import { TErrorSources } from '../interface/error'
+import config from '../config'
 
-  next()
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  //setting default values
+  let statusCode = 500
+  let message = 'Something went wrong!'
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ]
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
+  }
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    err: config.NODE_ENV === 'development' ? err : '',
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+  })
 }
 
 export default globalErrorHandler
